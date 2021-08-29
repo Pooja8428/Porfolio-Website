83bc3440b2ad4d17bf1f21c1f5995e7451ea5431
Pooja8428 <88193201+Pooja8428@users.noreply.github.com>
2021-08-29 14:51:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -29,39 +29,4 @@ $(document).ready(function(){
        $('ul').toggleClass("show");
        $('ul').toggleClass("active");
    });
-   // typing text animation script
-   var typed = new Typed(".typing", {
-       strings: ["Student"],
-       typeSpeed: 100,
-       backSpeed: 60,
-       loop: true
-   });
-   var typed = new Typed(".typing-2", {
-       strings: ["Student"],
-       typeSpeed: 100,
-       backSpeed: 60,
-       loop: true
-   });
-   // owl carousel script
-   $('.carousel').owlCarousel({
-       margin: 20,
-       loop: true,
-       autoplay: true,
-       autoplayTimeOut: 2000,
-       autoplayHoverPause: true,
-       responsive: {
-           0:{
-               items: 1,
-               nav: false
-           },
-           600:{
-               items: 2,
-               nav: false
-           },
-           1000:{
-               items: 3,
-               nav: false
-           }
-       }
-   });
 });
