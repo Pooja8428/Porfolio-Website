ff2549ab55a64ba8f352d665b274dff0fd12f0d9
Pooja8428 <88193201+Pooja8428@users.noreply.github.com>
2021-08-29 14:47:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,67 @@
 $(document).ready(function(){
-   $(document).scroll(function(){
-      if(this.scrollY > 20){
-          $('.navbar').addClass("sticky");
-      }else{
-          $('.navbar').removeClass("sticky");
-      }
+   $(window).scroll(function(){
+       // sticky navbar on scroll script
+       if(this.scrollY > 20){
+           $('.navbar').addClass("sticky");
+       }else{
+           $('.navbar').removeClass("sticky");
+       }
+       
+       // scroll-up button show/hide script 
+       if(this.scrollY > 500){
+           $('.scroll-up-btn').addClass("show");
+       }else{
+           $('.scroll-up-btn').removeClass("show");
+       }
    });
-});
-$(document).ready(function () {
-     $('.menu-btn').click(function () {
-        $('ul').toggleClass('show')
-     });
-  });
-
-$('.menu-btn').click(function(){
-    $('.navbar .ul').toggleClass(".active");
+   // slide-up script
+   $('.scroll-up-btn').click(function(){
+       $('html').animate({scrollTop: 0});
+       // removing smooth scroll on slide-up button click
+       $('html').css("scrollBehavior", "auto");
+   });
+   $('.navbar .menu li a').click(function(){
+       // applying again smooth scroll on menu items click
+       $('html').css("scrollBehavior", "smooth");
+   });
+   // toggle menu/navbar script
+   $('.menu-btn').click(function(){
+       $('ul').toggleClass("show");
+       $('ul').toggleClass("active");
+   });
+   // typing text animation script
+   var typed = new Typed(".typing", {
+       strings: ["Student"],
+       typeSpeed: 100,
+       backSpeed: 60,
+       loop: true
+   });
+   var typed = new Typed(".typing-2", {
+       strings: ["Student"],
+       typeSpeed: 100,
+       backSpeed: 60,
+       loop: true
+   });
+   // owl carousel script
+   $('.carousel').owlCarousel({
+       margin: 20,
+       loop: true,
+       autoplay: true,
+       autoplayTimeOut: 2000,
+       autoplayHoverPause: true,
+       responsive: {
+           0:{
+               items: 1,
+               nav: false
+           },
+           600:{
+               items: 2,
+               nav: false
+           },
+           1000:{
+               items: 3,
+               nav: false
+           }
+       }
+   });
 });
